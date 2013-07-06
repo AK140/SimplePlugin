23eb60467bc1d1e592a0e1c92b45fdeb1cc6e008
Lambo993 <Acer@RICHKY.Sumanli>
2013-07-06 20:16:23 +0700

    Revert "small-fixes"
    This reverts commit 75094ed5c8aa775247d818b602e49047b038f5b3.

--- src/io/github/lambo993/Commands/Commandfreeitem.java
+++ src/io/github/lambo993/Commands/Commandfreeitem.java
@@ -21,39 +21,28 @@ public class Commandfreeitem implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("FreeItem") && args.length == 2 ) {
-			if (sender instanceof Player) {
-				Player player = (Player) sender;
-				PlayerInventory inventory = player.getInventory();
-				ItemStack wood = new ItemStack(Material.WOOD, Integer.parseInt(args[1]));
-				ItemStack dirt = new ItemStack(Material.DIRT, Integer.parseInt(args[1]));
-				ItemStack stone = new ItemStack(Material.STONE, Integer.parseInt(args[1]));
-				ItemStack diamond = new ItemStack(Material.DIAMOND, Integer.parseInt(args[1]));
-				ItemStack cake = new ItemStack(Material.CAKE, Integer.parseInt(args[1]));
-				ItemStack cobblestone = new ItemStack(Material.COBBLESTONE , Integer.parseInt(args[0]));
-				if(args[0].equalsIgnoreCase("Wood")){
-					inventory.addItem(wood);
-				}
-				if(args[0].equalsIgnoreCase("Dirt")){
-					inventory.addItem(dirt);
-				}
-				if(args[0].equalsIgnoreCase("Stone")){
-					inventory.addItem(stone);
-				}
-				if(args[0].equalsIgnoreCase("Diamond")){
-					inventory.addItem(diamond);
-				}
-				if(args[0].equalsIgnoreCase("Cake")){
-					inventory.addItem(cake);
-				}
-				if(args[0].equalsIgnoreCase("Cobblestone")) {
-					inventory.addItem(cobblestone);
-				}
-			} else {
-				sender.sendMessage("§cError:§4 freeitem can only be used by player!");
+		if(cmd.getName().equalsIgnoreCase("FreeItem") && args.length == 1 ) {
+			Player player = (Player) sender;
+			PlayerInventory inventory = player.getInventory();
+			ItemStack woodstack = new ItemStack(Material.WOOD, 64);
+			ItemStack dirtstack = new ItemStack(Material.DIRT, 64);
+			ItemStack stonestack = new ItemStack(Material.STONE, 64);
+			ItemStack diamondstack = new ItemStack(Material.DIAMOND, 64);
+			if(args[0].equalsIgnoreCase("Wood")){
+				inventory.addItem(woodstack);
+			}
+			if(args[0].equalsIgnoreCase("Dirt")){
+				inventory.addItem(dirtstack);
+			}
+			if(args[0].equalsIgnoreCase("Stone")){
+				inventory.addItem(stonestack);
+			}
+			if(args[0].equalsIgnoreCase("Diamond")){
+				inventory.addItem(diamondstack);
 			}
 			return true;
 		}
 		return false;
 	}
+
 }
